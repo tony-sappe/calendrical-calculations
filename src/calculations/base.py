acf53c9da9e05d6ef47c9d94a31de27c29783ebd
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2022-10-02 00:18:46 -0600

    good progress

--- src/calculations/base.py
+++ src/calculations/base.py
@@ -1,5 +1,4 @@
-from typing import Optional
-
+from math import floor
 
 (
     JANUARY,
@@ -14,7 +13,9 @@ from typing import Optional
     OCTOBER,
     NOVEMBER,
     DECEMBER,
-) = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+) = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11)
+
+SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY = 0, 1, 2, 3, 4, 5, 6
 
 
 def rd(tee: int) -> int:
@@ -28,9 +29,10 @@ class DateFormatException(Exception):
 
 
 class Date:
-    year: int
-    month: int
-    day: int
+    _year: int
+    _month: int
+    _day: int
+    _fixed: int
 
     def __init__(self, year: int, month: int, day: int):
         raise NotImplementedError()
@@ -46,66 +48,140 @@ class Date:
 
 class Gregorian(Date):
     epoch: int = rd(1)
-    month_to_name = {
-        1: "January",
-        2: "February",
-        3: "March",
-        4: "April",
-        5: "May",
-        6: "June",
-        7: "July",
-        8: "August",
-        9: "September",
-        10: "October",
-        11: "November",
-        12: "December",
-    }
+    month_names = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+    day_names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
     def __init__(self, y: int, m: int, d: int):
         self.month_lengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        self.year = int(y)
-        self.month = int(m)
-        self.day = int(d)
+        self._year = int(y)
+        self._month = int(m) - 1
+        self._day = int(d)
 
         if self.is_leapyear:
-            self.month_lengths[self._month(FEBRUARY)] = 29
+            self.month_lengths[1] = 29
 
         # print(f"Gregorian({y}-{m}-{d})")
         self._verify()
 
+        # self._fixed = fixed_from_gregorian(self)
+
     def __repr__(self):
         return f"Gregorian({self.year:04}, {self.month:02}, {self.day:02})"
-
-    def _month(self, num: Optional[int] = None) -> int:
-        """Month numbers are 1-12 and lists are 0-indexed"""
-
-        if num is None:
-            return self.month - 1
-        return num - 1
 
     def _verify(self):
         """"""
 
-        if self.year == 0:
+        if self._year == 0:
             raise DateFormatException("No year 0 for Gregorian Calendar")
 
-        if self.month < 0 or self.month > 12:
-            raise DateFormatException(f"{self.month} falls outside of the 12 valid months")
+        if self._month < 0 or self._month > 11:
+            raise DateFormatException(f"{self.month} falls outside of the 1-12 valid months")
 
         if self.day < 0 or self.day > self.month_duration:
             raise DateFormatException(
-                f"{self.day} falls outside of {self.month_to_name[self.month]}'s {self.month_duration} days"
+                f"{self.day} falls outside of {self.month_name}'s {self.month_duration} days"
             )
+
+    @property
+    def year(self) -> int:
+        return self._year
+
+    @property
+    def month(self) -> int:
+        return self._month + 1
+
+    @property
+    def day(self) -> int:
+        return self._day
+
+    @property
+    def year_name(self) -> str:
+        postfix = "B.C."
+        if self._year > 0:
+            postfix = "A.D."
+
+        return f"{self._year} {postfix}"
+
+    @property
+    def month_name(self) -> str:
+        return self.month_names[self._month]
+
+    @property
+    def day_name(self) -> str:
+        """Pretty-print integer by adding the ordinal indicator
+
+        Found on StackOverflow
+            -> "tsnrhtdd"[(n//10%10!=1)*(n%10<4)*n%10::4])
+            https://stackoverflow.com/questions/9647202/ordinal-numbers-replacement
+        Which claims to use Gareth's CodeGolf solution:
+            -> "tsnrhtdd"[(i/10%10!=1)*(k<4)*k::4])
+            https://codegolf.stackexchange.com/questions/4707/outputting-ordinal-numbers-1st-2nd-3rd#answer-4712
+
+        This method is distributed under CC BY-SA 3.0
+            -> Attribution-ShareAlike 3.0 Unported (https://creativecommons.org/licenses/by-sa/3.0/legalcode)
+        """
+
+        x = "tsnrhtdd"[(self.day // 10 % 10 != 1) * (self.day % 10 < 4) * self.day % 10 :: 4]
+        return f"{self.day}{x}"
+
+    @property
+    def dow(self) -> str:
+        """Day number of Week"""
+        return day_of_week_from_fixed(self.fixed)
+
+    @property
+    def dow_name(self) -> str:
+        """Day of Week"""
+        return self.day_names[self.dow]
 
     @property
     def month_duration(self) -> int:
         """Obtain the number of days in the month"""
-        return self.month_lengths[self.month - 1]
+        return self.month_lengths[self._month]
 
     @property
     def is_leapyear(self) -> int:
         """True if the current year is a leap year"""
-        return gregorian_leap_year(self.year)
+        return gregorian_leap_year(self._year)
+
+    @property
+    def pretty_display(self) -> str:
+        return f"{self.dow_name} {self.month_name} {self.day_name}, {self.year_name}"
+
+    @property
+    def fixed(self):
+
+        if self.month <= 2:
+            factor = 0
+        elif self.is_leapyear:
+            factor = -1
+        else:
+            factor = -2
+
+        return (
+            self.epoch
+            - 1
+            + 365 * (self.year - 1)
+            + floor((self.year - 1) / 4)
+            - floor((self.year - 1) / 100)
+            + floor((self.year - 1) / 400)
+            + floor((1 / 12) * (367 * self.month - 362))
+            + factor
+            + self.day
+        )
 
 
 # def standard_year(date):
@@ -128,32 +204,30 @@ def gregorian_leap_year(year: int) -> bool:
     return year % 4 == 0 and not year % 400 in (100, 200, 300)
 
 
-# def fixed_from_gregorian(gdate):
-#     year = gdate.year
-#     month = gdate.month
-#     day = gdate.day
+def day_of_week_from_fixed(fixed_date: int) -> int:
+    return (fixed_date - rd(0) - SUNDAY) % 7
 
 
-# fixed-from-gregorian (g-date)
-#   ;; TYPE gregorian-date -> fixed-date
-#   ;; Fixed date equivalent to the Gregorian date $g-date$.
-#   (let* ((month (standard-month g-date))
-#          (day (standard-day g-date))
-#          (year (standard-year g-date)))
-#     (+ (1- gregorian-epoch); Days before start of calendar
-#        (* 365 (1- year)); Ordinary days since epoch
-#        (quotient (1- year)
-#                  4); Julian leap days since epoch...
-#        (-          ; ...minus century years since epoch...
-#         (quotient (1- year) 100))
-#        (quotient   ; ...plus years since epoch divisible...
-#         (1- year) 400)  ; ...by 400.
-#        (quotient        ; Days in prior months this year...
-#         (- (* 367 month) 362); ...assuming 30-day Feb
-#         12)
-#        (if (<= month 2) ; Correct for 28- or 29-day Feb
-#            0
-#          (if (gregorian-leap-year? year)
-#              -1
-#            -2))
-#        day)))          ; Days so far this month.
+def fixed_from_gregorian(gdate: Gregorian) -> int:
+    year = gdate.year
+    month = gdate.month
+    day = gdate.day
+
+    if month <= 2:
+        factor = 0
+    elif gdate.is_leapyear:
+        factor = -1
+    else:
+        factor = -2
+
+    return (
+        gdate.epoch
+        - 1
+        + 365 * (year - 1)
+        + floor((year - 1) / 4)
+        - floor((year - 1) / 100)
+        + floor((year - 1) / 400)
+        + floor((1 / 12) * (367 * month - 362))
+        + factor
+        + day
+    )
--- src/tests/gregorian.py
+++ src/tests/gregorian.py
@@ -1,16 +1,22 @@
 from ..calculations import *
 
+assert gregorian_leap_year(1900) is False, "❌"
+assert gregorian_leap_year(1999) is False, "❌"
+assert gregorian_leap_year(2000) is True, "❌"
+assert gregorian_leap_year(2001) is False, "❌"
+assert gregorian_leap_year(2002) is False, "❌"
+assert gregorian_leap_year(2003) is False, "❌"
+assert gregorian_leap_year(2004) is True, "❌"
+assert gregorian_leap_year(2005) is False, "❌"
 
-print(JANUARY)
 
-assert gregorian_leap_year(1900) is False
-assert gregorian_leap_year(1999) is False
-assert gregorian_leap_year(2000) is True
-assert gregorian_leap_year(2001) is False
-assert gregorian_leap_year(2002) is False
-assert gregorian_leap_year(2003) is False
-assert gregorian_leap_year(2004) is True
-assert gregorian_leap_year(2005) is False
+assert Gregorian.month_names[JANUARY] == "January", "❌"
+assert Gregorian.month_names[NOVEMBER] == "November", "❌"
+assert len(Gregorian.month_names) == 12, "❌"
+
+assert Gregorian.day_names[MONDAY] == "Monday", "❌"
+assert Gregorian.day_names[SATURDAY] == "Saturday", "❌"
+assert len(Gregorian.day_names) == 7, "❌"
 
 
 leapyear_dates = [(2000, 4, 1), (536, 3, 30), (2504, 9, 12), (4, 4, 4), (-400, 1, 1)]
@@ -18,19 +24,19 @@ leapyear_dates = [(2000, 4, 1), (536, 3, 30), (2504, 9, 12), (4, 4, 4), (-400, 1
 
 for d in leapyear_dates:
     G = Gregorian(d[0], d[1], d[2])
-    assert G.epoch == 1
-    assert G.year == d[0]
-    assert G.month == d[1]
-    assert G.day == d[2]
-    assert G.is_leapyear is True
-    print(f"{G} is VALID. ✅")
+    assert G.epoch == 1, "❌"
+    assert G.year == d[0], "❌"
+    assert G.month == d[1], "❌"
+    assert G.day == d[2], "❌"
+    assert G.is_leapyear is True, "❌"
+    print(f"✅ {G} is VALID -> {G.pretty_display}")
 
 
-invalid_dates = [("a", 4, 1), (1999, 2, 29), (0, 9, 7)]
+invalid_dates = [("a", 4, 1), (1999, 2, 29), (0, 10, 7), (2020, 9, 31), (2015, 12, -1), (2016, -3, 11), (2012, 0, 12), (2008, 0, 9)]
 
 for d in invalid_dates:
     try:
         G = Gregorian(d[0], d[1], d[2])
-        assert False, f"issue with {G}: month duration {G.month_duration}"
+        assert False, f"❌ issue with {G}: month duration {G.month_duration}"
     except (DateFormatException, ValueError):
-        print(f"({d[0]:>4}, {d[1]:>2}, {d[2]:>2}) is BOGUS! ✅")
+        print(f"✅ ({d[0]:>4}, {d[1]:>2}, {d[2]:>2}) is BOGUS!")
