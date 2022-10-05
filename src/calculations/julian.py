e8cee80a36c18e22196deb53b390d5280c7e2b94
Tony Sappe <22781949+tony-sappe@users.noreply.github.com>
2022-10-04 22:32:36 -0600

    added Julian calendar class

--- src/calculations/julian.py
+++ src/calculations/julian.py
@@ -6,8 +6,8 @@ from .third_party import get_ordinal_indicator
 from .base import Date, rd, day_of_week_from_fixed, DateFormatException
 
 
-class Gregorian(Date):
-    epoch: int = rd(Epoch.Gregorian)
+class Julian(Date):
+    epoch: int = rd(Epoch.Julian)
     month_names = [
         "January",
         "February",
@@ -46,24 +46,28 @@ class Gregorian(Date):
 
     def from_fixed(self, fixed_date):
         """Poor-man's Constructor when providing Rata Die Fixed Date"""
-        self._rata_die = floor(fixed_date)
+        # self._rata_die = floor(fixed_date)
+        self._rata_die = fixed_date
         self._date_from_fixed()
         return self
 
     def __repr__(self):
-        return f"Gregorian({self.year:04}, {self.month:02}, {self.day:02})"
+        return f"Julian({self.year:04}, {self.month:02}, {self.day:02})"
 
     def __add__(self, other) -> Date:
-        return Gregorian().from_fixed(self.fixed + int(other))
+        return Julian().from_fixed(self.fixed + int(other))
 
     def __sub__(self, other) -> Date:
-        return Gregorian().from_fixed(self.fixed - int(other))
+        return Julian().from_fixed(self.fixed - int(other))
 
     def __rsub__(self, other) -> Date:
-        return Gregorian().from_fixed(int(other) - self.fixed)
+        return Julian().from_fixed(int(other) - self.fixed)
 
     def _verify(self):
         """Verify the legitimacy of the provided YYYY-MM-DD"""
+
+        if self._year == 0:
+            raise DateFormatException(f"No year 0 in the Julian Calendar")
 
         if self._month < 0 or self._month > 11:
             raise DateFormatException(f"{self.month} falls outside of the 1-12 valid months")
@@ -87,7 +91,11 @@ class Gregorian(Date):
 
     @property
     def year_name(self) -> str:
-        return f"{self._year}"
+        postfix = "A.D."
+        if self._year < 0:
+            postfix = "B.C."
+
+        return f"{abs(self._year)} {postfix}"
 
     @property
     def month_name(self) -> str:
@@ -117,7 +125,7 @@ class Gregorian(Date):
     @property
     def is_leapyear(self) -> int:
         """True if the current year is a leap year"""
-        return gregorian_leap_year(self._year)
+        return julian_leap_year(self._year)
 
     @property
     def pretty_display(self) -> str:
@@ -125,7 +133,9 @@ class Gregorian(Date):
 
     @property
     def fixed(self):
-        prior_y = self.year - 1
+        y = self.year
+        if self.year < 0:
+            y += 1
 
         if self.month <= 2:
             february_correction = 0
@@ -137,8 +147,8 @@ class Gregorian(Date):
         sum_of_days_in_previous_months = (
             floor((367 * self.month - 362) / 12) + february_correction
         )
-        total_leap_days = floor((prior_y) / 4) - floor((prior_y) / 100) + floor((prior_y) / 400)
-        sum_of_days_in_previous_years = 365 * (self.year - 1) + total_leap_days
+        total_leap_days = floor((y - 1) / 4)
+        sum_of_days_in_previous_years = 365 * (y - 1) + total_leap_days
 
         return (
             (self.epoch - 1)
@@ -147,41 +157,31 @@ class Gregorian(Date):
             + self.day  # day of current month
         )
 
-    def _year_from_fixed(self) -> int:
-        """Gregorian Year from a Rata Die fixed-date"""
-
-        d0 = self._rata_die - self.epoch  # Prior Days
-        n400 = floor(d0 / 146097)  # Completed 400-year cycles
-        d1 = d0 % 146097  # Prior days not in n400
-        n100 = floor(d1 / 36524)  # 100-year cycles not in n400
-        d2 = d1 % 36524  # Prior days not in n400 or n100
-        n4 = floor(d2 / 1461)  # 4-year cycels not in n400 or n100
-        d3 = d2 % 1461  # Prior days not in n400, n100, or n4
-        n1 = floor(d3 / 365)  # years not in n400, n100, or n4
-
-        year = 400 * n400 + 100 * n100 + 4 * n4 + n1
-
-        if n100 != 4 and n1 != 4:  # If date falls in a leap year
-            year += 1
-
-        return year
-
     def _date_from_fixed(self):
-        """Calculate the Gregorian YYYY-MM-DD from a fixed-date"""
+        """Calculate the Julian YYYY-MM-DD from a fixed-date"""
 
-        self._year = self._year_from_fixed()
-        prior_days = self._rata_die - Gregorian().from_date(self._year, JANUARY, 1).fixed
+        approx = floor((4 * (self._rata_die - self.epoch) + 1464) / 1461)
+        if approx <= 0:
+            self._year = approx - 1
+        else:
+            self._year = approx
 
-        if self._rata_die < Gregorian().from_date(self._year, MARCH, 1).fixed:
+        if self._rata_die < Julian().from_date(self.year, MARCH, 1).fixed:
             correction = 0
         elif self.is_leapyear:
             correction = 1
         else:
             correction = 2
 
+        prior_days = self._rata_die - Julian().from_date(self.year, JANUARY, 1).fixed
+
         self._month = floor((12 * (prior_days + correction) + 373) / 367) - 1
-        self._day = self._rata_die - Gregorian().from_date(self._year, self.month, 1).fixed + 1
+        self._day = floor(self._rata_die - Julian().from_date(self.year, self.month, 1).fixed + 1)
 
 
-def gregorian_leap_year(year: int) -> bool:
-    return year % 4 == 0 and not year % 400 in (100, 200, 300)
+def julian_leap_year(year: int) -> bool:
+    if year > 0:
+        x = 0
+    else:
+        x = 3
+    return year % 4 == x
